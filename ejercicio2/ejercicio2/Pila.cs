7ccf16c64b0fe276a7c42cacd3cc9efc8a16d981
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-02-22 16:23:49 -0600

    Ejercicio 2

---
+++ ejercicio2/ejercicio2/Pila.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Pila
+    {
+        public nodo tope;
+
+        public Pila()
+        {
+            tope = null;
+        }
+
+        public void Push(int valor)
+        {
+            nodo aux = new nodo();
+            aux.info = valor;
+            int contador = 0;
+            
+
+            if(tope == null)
+            {
+                tope = aux;
+                aux.sgte = null;
+            }
+            else
+            {
+                contador = Contar();
+                if(contador > 5)
+                {
+                    Console.WriteLine("No se pueden agregar más de 5 contenedores");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    aux.sgte = tope;
+                    tope = aux;
+                }
+            }
+        }
+
+        public int Contar()
+        {
+            nodo puntero = new nodo();
+            puntero = tope; ;
+            int contador = 2;
+
+            while(puntero.sgte != null)
+            {
+                contador++;
+                puntero = puntero.sgte;
+            }
+            return contador;
+        }
+
+        public void PopValor(int valor, Pila pila_aux, Pila pila)
+        {
+            nodo aux; aux = tope;
+            int var = 0;
+            if(aux == null)
+            {
+                Console.WriteLine("Almacen vacío");
+            }
+            else
+            {
+                Console.WriteLine("Contenedor retirado");
+                Console.ReadLine();
+                while (aux.info != valor && aux.sgte != null)
+                {
+                    var = aux.info;
+                    aux = aux.sgte;
+                    pila_aux.Push(var);
+                    pila.Pop();
+                }
+                if(aux.sgte == null)
+                {
+                    Console.WriteLine("El contenedor no existe");
+                    Console.ReadLine();
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Enter para continuar...");
+            Console.ReadLine();
+            pila.Pop();
+            pila_aux.Retornar(pila_aux, pila);
+        }
+
+        public void Retornar(Pila pila_aux, Pila pila)
+        {
+            nodo aux; aux = tope;
+            int var = 0;
+            while(aux != null)
+            {
+                var = aux.info;
+                aux = aux.sgte;
+                pila.Push(var);
+                pila_aux.Pop();
+            }
+        }
+
+        public int Pop()
+        {
+            int valor = 0;
+            if (tope == null)
+                Console.WriteLine("Almacen vacío");
+            else
+            {
+                valor = tope.info;
+                tope = tope.sgte;
+            }
+            return valor;
+        }
+
+        public void mostrar()
+        {
+            nodo puntero; puntero = tope;
+            if(puntero == null)
+            {
+                Console.WriteLine("Almacen vacío");
+            }
+            else
+            {
+                Console.WriteLine("{0}", puntero.info);
+                while(puntero.sgte!= null)
+                {
+                    puntero = puntero.sgte;
+                    Console.WriteLine("{0}", puntero.info);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila almacen = new Pila();
+            Pila almacen_aux = new Pila();
+
+            int op = 0;
+            int valor = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("---ALMACEN---");
+                Console.WriteLine("1. Agregar contenedor a almacen");
+                Console.WriteLine("2. Retirar contenedor de almacen");
+                Console.WriteLine("3. Mirar estado de almacen (pila principal)");
+                Console.WriteLine("4. Salir");
+                Console.Write("Escoger una opción: ");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.Write("Agregar contenedor (No más de 5 contenedores): ");
+                        valor = int.Parse(Console.ReadLine());
+                        almacen.Push(valor);
+                        break;
+                    case 2:
+                        Console.Write("Retirar contenedor: ");
+                        valor = int.Parse(Console.ReadLine());
+                        almacen.PopValor(valor, almacen_aux, almacen);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("ALMACEN PRINCIPAL");
+                        almacen.mostrar();
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                } 
+            } while (op != 4);
+        }
+    }
+}
